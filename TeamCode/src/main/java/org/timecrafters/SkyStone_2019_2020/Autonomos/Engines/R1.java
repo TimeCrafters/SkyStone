23a4cb0290ae051b8e3df2e6c27de265f1f2075b
Cyberarm <matthewlikesrobots@gmail.com>
2019-11-15 17:42:08 -0600

    Updated R1

--- TeamCode/src/main/java/org/timecrafters/SkyStone_2019_2020/Autonomos/Engines/R1.java
+++ TeamCode/src/main/java/org/timecrafters/SkyStone_2019_2020/Autonomos/Engines/R1.java
@@ -3,9 +3,13 @@ package org.timecrafters.SkyStone_2019_2020.Autonomos.Engines;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.cyberarm.NeXT.StateConfiguration;
+import org.timecrafters.SkyStone_2019_2020.Autonomos.States.Arms;
+import org.timecrafters.SkyStone_2019_2020.Autonomos.States.Crane;
 import org.timecrafters.SkyStone_2019_2020.Autonomos.States.Fingers;
 import org.timecrafters.SkyStone_2019_2020.Autonomos.States.DirectionDrive;
 import org.timecrafters.SkyStone_2019_2020.Autonomos.States.Face;
+import org.timecrafters.SkyStone_2019_2020.Autonomos.States.Lift;
+import org.timecrafters.SkyStone_2019_2020.Autonomos.States.LiftZero;
 import org.timecrafters.engine.Engine;
 
 @Autonomous (name = "R1", group = "R")
@@ -19,8 +23,10 @@ public class R1 extends Engine {
         addState(new DirectionDrive(this, stateConfiguration, "R1c_move"));
         addState(new Face(this, stateConfiguration, "R1d"));
         addState(new DirectionDrive(this, stateConfiguration, "R1e"));
+        addState(new Crane(this, stateConfiguration,"R1e_carriage_out"));
+        addState(new LiftZero(this, stateConfiguration, "R1e_lower"));
         addState(new Fingers(this, stateConfiguration, "R1f"));
         addState(new DirectionDrive(this, stateConfiguration, "R1g"));
-        addState(new Face(this, stateConfiguration, "R1h"));
+        addState(new DirectionDrive(this, stateConfiguration, "R1h"));
     }
 }
