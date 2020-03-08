708d3bec81360926aa5c4eee734d03fffc591061
scottbadger777 <37743855+scottbadger777@users.noreply.github.com>
2020-03-08 13:44:18 -0500

    New Drive 1   yeeetettetrcv

---
+++ TeamCode/src/main/java/org/timecrafters/Summer_2020/Thomas_summer2020/engineimuthing.java
@@ -0,0 +1,4 @@
+package org.timecrafters.Summer_2020.Thomas_summer2020;
+
+public class engineimuthing {
+}
---
+++ TeamCode/src/main/java/org/timecrafters/Summer_2020/Thomas_summer2020/stateimuthing.java
@@ -0,0 +1,38 @@
+package org.timecrafters.Summer_2020.Thomas_summer2020;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+
+import org.timecrafters.engine.State;
+
+public class stateimuthing extends State {
+
+    private BNO055IMU IMU;
+
+    @Override
+    public void init() {
+        IMU = engine.hardwareMap.get(BNO055IMU.class, "imu");
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled = false;
+
+        IMU.initialize(parameters);
+
+
+    }
+
+    @Override
+    public void telemetry() {
+        engine.telemetry .addData("rotation X", IMU.getAngularOrientation().firstAngle);
+        engine.telemetry .addData("rotation Z", IMU.getAngularOrientation().firstAngle);
+        engine.telemetry .addData("rotation Y", IMU.getAngularOrientation().firstAngle);
+
+        @Override
+    }
+    public void exec() throws InterruptedException {
+
+    }
+}
