8d43b18dc8d962598baa7ad8047d54d4407457cc
yeet-mp4 <thomasharkey67@gmail.com>
2020-05-02 13:25:38 -0500

    12345

--- TeamCode/src/main/java/org/timecrafters/Summer_2020/Thomas_summer2020/drift_thing_engine.java
+++ TeamCode/src/main/java/org/timecrafters/Summer_2020/Thomas_summer2020/drift_thing_engine.java
@@ -1,9 +1,14 @@
 package org.timecrafters.Summer_2020.Thomas_summer2020;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import org.cyberarm.NeXT.StateConfiguration;
 import org.timecrafters.engine.Engine;
-
+@TeleOp (name = "12345")
 public class drift_thing_engine extends Engine {
+
+
+
     @Override
     public void setProcesses() {
         StateConfiguration stateConfiguration= new StateConfiguration();
